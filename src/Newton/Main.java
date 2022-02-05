ada0522fd14946ba68e60eb9387a8fb2cda63d2a
Ethan Hodges <hodgese5252@gmail.com>
2022-02-04 18:26:04 -0700

    Edit by Ethan

--- src/Newton/Main.java
+++ src/Newton/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	System.out.println("Hello, my name is Kimball");
+    System.out.println("Hello Kimball, my name is Ethan!");
     }
 }
