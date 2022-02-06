5f920856b3de7dd65f64d8742f1a93d0589b160e
Jared Joshua Balajadia <46180962+Zukashikute@users.noreply.github.com>
2022-02-06 11:22:00 +0800

    Update Main.java

--- src/Newton/Main.java
+++ src/Newton/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello, my name is Kimball");
     System.out.println("Hello Kimball, my name is Ethan!");
+	System.out.println("Hello Kimball, A man named Jared is in your file.!");
     }
 }
